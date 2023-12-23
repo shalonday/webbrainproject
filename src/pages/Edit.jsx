fe04e1a9c9651b6826ffac7ffb31d1d8322963d5
shalonday <69411982+shalonday@users.noreply.github.com>
2023-12-23 10:39:28 +0800

    Remove misplaced comment

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -19,11 +19,6 @@ function Edit() {
   const [currentNode, setCurrentNode] = useState(null);
   const [isModuleModalVisible, setIsModuleModalVisible] = useState(false);
 
-  // Generate the path upon opening this page. I do this here instead of at
-  // the Generate Path button on Search.jsx so that the path is based on url
-  // params, making the same path easily shareable.
-  // Scenario 1: A user w/o an account accesses the page and sees the path w/o any active nodes
-  // Scenario 2: activeNodesIdList is updated from user account (or some uploaded json?) then treeWithActiveNodes is set from this
   useEffect(function initializeEditPagePart1() {
     getNodesById(nodeIds);
   }, []);
