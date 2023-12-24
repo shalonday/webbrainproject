b7c64bce0e6c110c95ae3e708d42f139634b2822
shalonday <69411982+shalonday@users.noreply.github.com>
2023-12-24 16:43:04 +0800

    Use process.env
    So that the Netlify build can access the environment variables set on
    Netlify. I can just change back to import.env when testing locally.

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = process.env.SUPABASE_URL; //import.meta
+const supabaseKey = process.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
