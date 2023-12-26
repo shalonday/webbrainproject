70a0c045c040204891254f13bb1d84a37a57ffef
shalonday <69411982+shalonday@users.noreply.github.com>
2023-12-27 07:33:55 +0800

    Change to using React Query instead of Context

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,37 +1,49 @@
 import styles from "./Edit.module.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useSkillTreesContext } from "../contexts/SkillTreesContext";
 import ModuleModal from "../components/edit/ModuleModal";
 import { useNavigate, useParams } from "react-router-dom";
 import EditPageChart from "../components/edit/EditPageChart";
 import MainButton from "../components/MainButton";
+import { useQuery } from "@tanstack/react-query";
+import { fetchUniversalTree } from "../services/apiTrees";
 import Loader from "../components/Loader";
 
+// String, Tree -> Tree (nodes only)
+// get nodes by id without fetching from db
+function getNodesById(nodeIdString, tree) {
+  if (nodeIdString === "blank") return { nodes: [], links: [] };
+  else {
+    const idsArray = nodeIdString.split(",");
+    const nodesArray = tree.nodes.filter((node) => idsArray.includes(node.id));
+    return { nodes: nodesArray, links: [] };
+  }
+}
+
 function Edit() {
-  const { displayedTree, mergeTree, getNodesById, isLoading, error } =
-    useSkillTreesContext();
+  const {
+    isLoading,
+    data: universalTree,
+    error,
+  } = useQuery({
+    queryKey: ["universalTree"],
+    queryFn: fetchUniversalTree,
+  });
+
+  const { mergeTree } = useSkillTreesContext();
 
   const navigate = useNavigate();
-  const { nodeIds } = useParams(); // string of node IDs separated by &
+  const { nodeIds } = useParams(); // string of node IDs separated by ","
 
-  const [currentTree, setCurrentTree] = useState(displayedTree);
+  const [currentTree, setCurrentTree] = useState(
+    universalTree
+      ? getNodesById(nodeIds, universalTree)
+      : { nodes: [], links: [] }
+  );
   const [selectedNodes, setSelectedNodes] = useState([]);
   const [currentNode, setCurrentNode] = useState(null);
   const [isModuleModalVisible, setIsModuleModalVisible] = useState(false);
 
-  useEffect(function initializeEditPagePart1() {
-    getNodesById(nodeIds);
-  }, []);
-
-  useEffect(
-    function initializeEditPagePart2() {
-      setCurrentTree(displayedTree);
-    },
-    [displayedTree]
-  );
-
-  console.log(displayedTree);
-  console.log(currentTree);
   async function handleSubmit() {
     // validation?
     // merge currentTree to database tree if validation passes
@@ -57,7 +69,7 @@ function Edit() {
           </div>
           <p>{currentNode?.description}</p>
         </div>
-        {isLoading && <Loader />}
+        {isLoading && nodeIds !== "blank" && <Loader />}
         {error && <h1>{error}</h1>}
         {!isLoading && !error && currentTree && (
           <EditPageChart
