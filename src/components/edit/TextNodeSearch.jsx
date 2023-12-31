58fbda5e4ed35d1c85634fd1feac963a5c5438b5
shalonday <69411982+shalonday@users.noreply.github.com>
2023-12-31 14:44:05 +0800

    Fix some styles

--- src/components/edit/TextNodeSearch.jsx
+++ src/components/edit/TextNodeSearch.jsx
@@ -21,27 +21,9 @@ function TextNodeSearch({ tempNodes, setTempNodes, currentTree }) {
         <li key={index} className={styles.nodeInputGroup}>
           <span onClick={() => handleDeleteTempItem(index)}>&#10005;</span>
           <CreatableSelect
-            style={{ flex: 1 }}
+            className={styles.nodeSelect}
             placeholder="Select a node or type to create a new node..."
-            styles={{
-              control: (baseStyles) => ({
-                ...baseStyles,
-                backgroundColor: "black",
-              }),
-              menu: (baseStyles) => ({
-                ...baseStyles,
-                backgroundColor: "black",
-                border: "1px solid white",
-              }),
-              option: (baseStyles) => ({
-                ...baseStyles,
-                backgroundColor: "black",
-              }),
-              singleValue: (baseStyles) => ({
-                ...baseStyles,
-                color: "white",
-              }),
-            }}
+            styles={textSearchStyles}
             options={currentTreeNodeOptions}
             onChange={(e) => {
               setTempNodes((array) =>
@@ -57,5 +39,25 @@ function TextNodeSearch({ tempNodes, setTempNodes, currentTree }) {
     </>
   );
 }
+
+const textSearchStyles = {
+  control: (baseStyles) => ({
+    ...baseStyles,
+    backgroundColor: "black",
+  }),
+  menu: (baseStyles) => ({
+    ...baseStyles,
+    backgroundColor: "black",
+    border: "1px solid white",
+  }),
+  option: (baseStyles) => ({
+    ...baseStyles,
+    backgroundColor: "black",
+  }),
+  singleValue: (baseStyles) => ({
+    ...baseStyles,
+    color: "white",
+  }),
+};
 
 export default TextNodeSearch;
