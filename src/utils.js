059fe3ff4c820a9afd1d914781de28ddc58dd96b
shalonday <69411982+shalonday@users.noreply.github.com>
2023-12-31 14:53:54 +0800

    Add copyright and license notices
    Necessary for GNU GPL licensed repos according to:
    https://www.gnu.org/licenses/gpl-howto.en.html

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,20 @@
+/* 
+Copyright 2023, Salvador Pio Alonday
+
+This file is part of The Online Brain Project
+
+The Online Brain Project is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+The Online Brain Project is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with The Online
+Brain Project. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 //generate uuid; retrieved from https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid 4Sep2023
 export function uuidv4() {
   return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
