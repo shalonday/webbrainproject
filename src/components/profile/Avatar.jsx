e64e1334b91c8ad810fdc38f6eba3896468eb719
shalonday <69411982+shalonday@users.noreply.github.com>
2024-01-06 17:34:27 +0800

    Set basic styles and structure

--- src/components/profile/Avatar.jsx
+++ src/components/profile/Avatar.jsx
@@ -1,5 +1,21 @@
-function Avatar() {
-  return <div>Avatar</div>;
+import styled from "styled-components";
+
+const ProfilePic = styled.img`
+  display: block;
+  margin: 0 auto;
+`;
+
+const UserName = styled.h1`
+  text-align: center;
+`;
+
+function Avatar({ user }) {
+  return (
+    <div>
+      <ProfilePic src="/src/assets/avatar-placeholder.svg" />
+      <UserName>{user?.name ? user.name : "-"}</UserName>
+    </div>
+  );
 }
 
 export default Avatar;
