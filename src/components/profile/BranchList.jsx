c8682fb5d53994a28cc67c1b8eb6513ee4a41908
shalonday <69411982+shalonday@users.noreply.github.com>
2024-01-06 19:35:17 +0800

    Fix BranchList width for mobile

--- src/components/profile/BranchList.jsx
+++ src/components/profile/BranchList.jsx
@@ -2,18 +2,20 @@ import styled from "styled-components";
 
 const StyledBranchList = styled.ul`
   margin: 0 auto 30px;
+  padding: 10px;
   border: 1px solid white;
   border-radius: 8px;
   box-sizing: border-box;
+
   list-style: none;
-  padding: 10px;
+
   font-size: 1em;
   line-height: 20px;
   position: relative;
   touch-action: manipulation;
   transition: box-shadow 0.2s, -ms-transform 0.1s, -webkit-transform 0.1s,
     transform 0.1s;
-  width: 50%;
+  width: 80%;
 `;
 
 function BranchList({ children, title }) {
