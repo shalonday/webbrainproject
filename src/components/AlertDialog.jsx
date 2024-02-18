c30e76def20c91abe413959e377aa68699beec25
shalonday <69411982+shalonday@users.noreply.github.com>
2024-02-18 18:28:14 +0800

    Add DeleteModal
    DeleteModal is necessary to ask user for confirmation before they
    delete a component of the tree they're editing.

---
+++ src/components/AlertDialog.jsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function AlertDialog({open, setOpen, title, negBtnText, posBtnText, onNegBtnClick, onPosBtnClick, children}) {
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {title}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {children}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="outlined" onClick={onNegBtnClick}>{negBtnText}</Button>
+          <Button variant="outlined" onClick={onPosBtnClick}>
+            {posBtnText}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
---
+++ src/components/edit/DeleteModal.jsx
@@ -0,0 +1,22 @@
+import AlertDialog from "../AlertDialog";
+
+function DeleteModal({open, setOpen, currentNode, setCurrentTree}) {
+  function handleNegBtnClick(){
+    setOpen(false);
+  }
+  function handlePosBtnClick(){
+    setCurrentTree((tree) => {
+      const newTree = {
+        nodes: tree.nodes.filter(node => node.id !== currentNode.id ),
+        links: tree.links.filter(link => link.source !== currentNode.id && link.target !== currentNode.id), // filter out the links that have this node as target or as source (otherwise D3 will complain)
+      };
+      return newTree;
+    });
+    setOpen(false);
+  }
+  return <AlertDialog open={open} setOpen={setOpen} title="Delete this node?" negBtnText={"I Changed My Mind"} posBtnText={"Delete"} onNegBtnClick={handleNegBtnClick} onPosBtnClick = {handlePosBtnClick}>
+      Note that this deletes any links attached to the node.
+    </AlertDialog>;
+}
+
+export default DeleteModal;
