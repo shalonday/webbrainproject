ae8e4c93ad39c0716809b017d8e748fe32964a96
Salvador Pio Alonday <salvadorpio.alonday@hcl-software.com>
2025-06-07 17:08:50 +0800

    Add red tests for Search and Tree

---
+++ src/pages/Search.test.js
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import Search from "./Search";
+import { expect, test } from "vitest";
+
+test("User can search for a desired skill keyword", () => {
+  // render(<App />);
+  // const headingElement = screen.getByText(/learn react/i);
+  // expect(headingElement).not.toBeInTheDocument();
+    throw new Error;
+});
+
+test("After a user confirms the keyword they're searching for, the results are adequately highlighted", () => {
+    throw new Error;
+});
+
+test("User can pick a highlighted skill and generate a path to it", () => {
+	throw new Error;
+});
+
+test("User can pick a highlighted URL and generate a path to it", () => {
+	throw new Error;
+});
+
+test("There is a way to redirect user to the path page", () => {
+	throw new Error;
+});
---
+++ src/pages/Tree.test.js
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import Tree from "./Tree";
+import { expect, test } from "vitest";
+
+test("User sees a clear visualization of the learning path they asked for", () => {
+  // render(<App />);
+  // const headingElement = screen.getByText(/learn react/i);
+  // expect(headingElement).not.toBeInTheDocument();
+    throw new Error;
+});
+
+test("If the only highlighted node is the root node, the user is invited to take a series of skill checks to determine the edge skill", () => {
+    throw new Error;
+});
+
+test("There is an easy way for the user to proceed along the learning path in a clear linear manner", () => {
+    throw new Error;
+});
+
+test("The user's progress is clearly highlighted", () => {
+    throw new Error;
+});
+
+test("Successful skill checks highlight a skill node", () => {
+    throw new Error;
+});
+
+test("Highlighted paths and nodes are saved locally", () => {
+    throw new Error;
+});
