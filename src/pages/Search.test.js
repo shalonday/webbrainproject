aa11fa2990b790d820104c701989c5a36db6ebe2
Salvador Pio Alonday <salvadorpio.alonday@hcl-software.com>
2025-06-11 12:27:12 +0800

    Add tests for Search Bar

--- src/pages/Search.test.js
+++ src/pages/Search.test.js
@@ -1,8 +1,34 @@
 import { render, screen } from "@testing-library/react";
 import Search from "./Search";
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 
-test("User can search for a desired skill keyword", () => {
+describe("Search input box", () => {
+
+  test("Typing without having clicked on the search bar doesn't update the search bar input", () => {
+    //render(<Search/>);
+    throw new Error;
+  });
+
+  test("Search bar can be highlighted by clicking on it", () => {
+    //render(<Search/>);
+    throw new Error;
+  });
+
+  test("Search bar can be highlighted by tabbing to it", () => {
+    //render(<Search/>);
+    throw new Error;
+  });
+
+  test("Typing while the search bar is highlighted updates the text in it", () => {
+    //render(<Search/>);
+    throw new Error;
+  });
+  
+  test("Pressing the Enter key while the input box is highlighted confirms the search", () => {
+    //render(<Search/>);
+    throw new Error;
+  });
+
   // render(<App />);
   // const headingElement = screen.getByText(/learn react/i);
   // expect(headingElement).not.toBeInTheDocument();
