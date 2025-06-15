80f44f732ee402f87024ad6afdb4f02301f579d8
Salvador Pio Alonday <salvadorpio.alonday@hcl-software.com>
2025-06-15 14:33:23 +0800

    Add stub tests for Path page

--- src/pages/Tree.test.js
+++ src/pages/Tree.test.js
@@ -1,30 +1,40 @@
 import { render, screen } from "@testing-library/react";
 import Tree from "./Tree";
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 
-test("User sees a clear visualization of the learning path they asked for", () => {
-  // render(<App />);
-  // const headingElement = screen.getByText(/learn react/i);
-  // expect(headingElement).not.toBeInTheDocument();
-    throw new Error;
+describe("Branch Page Path Display", () => {
+    test("User sees a clear visualization of the learning path they asked for", () => {
+        // render(<App />);
+        // const headingElement = screen.getByText(/learn react/i);
+        // expect(headingElement).not.toBeInTheDocument();
+        throw new Error;
+    });
+
+
+    test("The user's progress is clearly highlighted", () => {
+        throw new Error;
+    });
+
+    test("Highlighted paths and nodes are saved locally", () => {
+        throw new Error;
+    });
 });
 
-test("If the only highlighted node is the root node, the user is invited to take a series of skill checks to determine the edge skill", () => {
-    throw new Error;
+describe("Initial Skill Check Prompt", () => {
+
+    test("If the only highlighted node is the root node, the user is invited to take a series of skill checks to determine the edge skill", () => {
+        throw new Error;
+    });
+
+    test("Successful skill checks highlight a skill node", () => {
+        throw new Error;
+    });
 });
 
-test("There is an easy way for the user to proceed along the learning path in a clear linear manner", () => {
-    throw new Error;
+describe("Continue Button", () => {
+    
+    test("There is an easy way for the user to proceed along the learning path in a clear linear manner", () => {
+        throw new Error;
+    });
 });
 
-test("The user's progress is clearly highlighted", () => {
-    throw new Error;
-});
-
-test("Successful skill checks highlight a skill node", () => {
-    throw new Error;
-});
-
-test("Highlighted paths and nodes are saved locally", () => {
-    throw new Error;
-});
