c1168f6cdaa716e47487865478959c891458ba58
Salvador Pio Alonday <salvadorpio.alonday@hcl-software.com>
2025-06-26 12:54:19 +0800

    Add 4-space indent rule

--- eslint.config.js
+++ eslint.config.js
@@ -6,24 +6,25 @@ import globals from "globals";
 
 
 export default defineConfig([
-  { 
-    extends: ["js/all"], 
-    files: ["**/*.{js,mjs,cjs,jsx}"], 
-    languageOptions: { globals: globals.browser }, 
-    plugins: { js, import: importPlugin },
-    rules: {
-      ...importPlugin.flatConfigs.recommended.rules,
-      "no-inline-comments": "off",
-      "sort-imports": "off", // turn off this core rule, prioritizing rules from eslint-plugin-import instead, to prevent conflicts
-      "import/order": [
-        'error',
-        {
-          alphabetize: { order: 'asc', caseInsensitive: true },
-          'newlines-between': 'never'
+    { 
+        extends: ["js/all"], 
+        files: ["**/*.{js,mjs,cjs,jsx}"], 
+        languageOptions: { globals: globals.browser }, 
+        plugins: { js, import: importPlugin },
+        rules: {
+            ...importPlugin.flatConfigs.recommended.rules,
+            "import/order": [
+                'error',
+                {
+                    alphabetize: { order: 'asc', caseInsensitive: true },
+                    'newlines-between': 'never'
+                }
+            ],
+            "indent": ["error", 4],
+            "no-inline-comments": "off",
+            "sort-imports": "off", // Turn off this core rule, prioritizing rules from eslint-plugin-import instead, to prevent conflicts
         }
-      ]
-    }
-  }, 
-  pluginReact.configs.flat.all,
-  pluginReact.configs.flat['jsx-runtime'], // Remove 'React must be in scope' errors in files that use jsx
+    }, 
+    pluginReact.configs.flat.all,
+    pluginReact.configs.flat['jsx-runtime'], // Remove 'React must be in scope' errors in files that use jsx
 ]);
