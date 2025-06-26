fe4866af688785193de4568351732b69d70daca8
Salvador Pio Alonday <salvadorpio.alonday@hcl-software.com>
2025-06-26 12:36:35 +0800

    Add eslint-plugin-import recommended and import/order

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
+import importPlugin from 'eslint-plugin-import'
 import pluginReact from "eslint-plugin-react";
 import globals from "globals";
 
@@ -9,9 +10,18 @@ export default defineConfig([
     extends: ["js/all"], 
     files: ["**/*.{js,mjs,cjs,jsx}"], 
     languageOptions: { globals: globals.browser }, 
-    plugins: { js },
+    plugins: { js, import: importPlugin },
     rules: {
-      "no-inline-comments": "off"
+      ...importPlugin.flatConfigs.recommended.rules,
+      "no-inline-comments": "off",
+      "sort-imports": "off", // turn off this core rule, prioritizing rules from eslint-plugin-import instead, to prevent conflicts
+      "import/order": [
+        'error',
+        {
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          'newlines-between': 'never'
+        }
+      ]
     }
   }, 
   pluginReact.configs.flat.all,
