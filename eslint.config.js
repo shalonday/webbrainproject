1f9ffe108ef7b0dbd1478c89a8f8f6b37e9a2480
shalonday <alondaysp@gmail.com>
2025-07-04 13:04:58 +0800

    Modify func-style, jsx-newline, no-multi-comp

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,20 @@
+/* 
+Copyright 2023, 2025 Salvador Pio Alonday
+
+This file is part of The Web Brain Project
+
+The Web Brain Project is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+The Web Brain Project is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with The Web
+Brain Project. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import importPlugin from 'eslint-plugin-import'
@@ -10,9 +27,11 @@ export default defineConfig([
         extends: ["js/all"], 
         files: ["**/*.{js,mjs,cjs,jsx}"], 
         languageOptions: { globals: globals.browser }, 
-        plugins: { js, import: importPlugin },
+        plugins: { import: importPlugin, js,  },
         rules: {
             ...importPlugin.flatConfigs.recommended.rules,
+            ...pluginReact.configs.flat.all,
+            "func-style": ["warn", "declaration"],
             "import/order": [
                 'error',
                 {
@@ -22,9 +41,10 @@ export default defineConfig([
             ],
             "indent": ["error", 4],
             "no-inline-comments": "off",
+            "react/jsx-newline": ["warn", { "allowMultilines": false, "prevent": true,  }],
+            "react/no-multi-comp": "off",
             "sort-imports": "off", // Turn off this core rule, prioritizing rules from eslint-plugin-import instead, to prevent conflicts
         }
-    }, 
-    pluginReact.configs.flat.all,
+    },
     pluginReact.configs.flat['jsx-runtime'], // Remove 'React must be in scope' errors in files that use jsx
 ]);
