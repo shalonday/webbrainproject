e0cab1d401da1db12366957dbf79205d2ecd4cd4
shalonday <alondaysp@gmail.com>
2025-07-04 13:44:19 +0800

    Resolve errors in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -16,35 +16,37 @@ Brain Project. If not, see <https://www.gnu.org/licenses/>.
 */
 
 import js from "@eslint/js";
+// eslint-disable-next-line import/no-unresolved
 import { defineConfig } from "eslint/config";
 import importPlugin from 'eslint-plugin-import'
 import pluginReact from "eslint-plugin-react";
 import globals from "globals";
 
+const numIndentSpaces = 4;
 
 export default defineConfig([
+    pluginReact.configs.flat.all,
+    pluginReact.configs.flat['jsx-runtime'], // Remove 'React must be in scope' errors in files that use jsx
     { 
         extends: ["js/all"], 
         files: ["**/*.{js,mjs,cjs,jsx}"], 
         languageOptions: { globals: globals.browser }, 
-        plugins: { import: importPlugin, js,  },
+        plugins: { import: importPlugin, js },
         rules: {
             ...importPlugin.flatConfigs.recommended.rules,
-            ...pluginReact.configs.flat.all,
             "func-style": ["warn", "declaration"],
             "import/order": [
                 'error',
                 {
-                    alphabetize: { order: 'asc', caseInsensitive: true },
+                    alphabetize: { caseInsensitive: true, order: 'asc',  },
                     'newlines-between': 'never'
                 }
             ],
-            "indent": ["error", 4],
+            "indent": ["error", numIndentSpaces],
             "no-inline-comments": "off",
             "react/jsx-newline": ["warn", { "allowMultilines": false, "prevent": true,  }],
             "react/no-multi-comp": "off",
             "sort-imports": "off", // Turn off this core rule, prioritizing rules from eslint-plugin-import instead, to prevent conflicts
         }
     },
-    pluginReact.configs.flat['jsx-runtime'], // Remove 'React must be in scope' errors in files that use jsx
 ]);
