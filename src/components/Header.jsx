59deb43b66cecf7216228b969d38b1dd9c5dc158
shalonday <alondaysp@gmail.com>
2025-07-21 11:08:30 +0800

    Remove useUser

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { HiUser } from "react-icons/hi";
-import { useUser } from "../hooks/useUser";
 
 const StyledHeader = styled.header`
   background-color: transparent;
@@ -27,35 +26,9 @@ const MenuItem = styled.li`
 
 function Header() {
   const navigate = useNavigate();
-  const [areOptionsVisible, setAreOptionsVisible] = useState(false);
-
-  const { user } = useUser();
-
-  function toggleOptions() {
-    setAreOptionsVisible((val) => !val);
-  }
-
-  function handleLogoutClick() {
-    logout();
-  }
   return (
     <StyledHeader>
       <H1 onClick={() => navigate("/")}>The Web Brain Project</H1>
-      <IconDiv>{user && <HiUser onClick={toggleOptions} />}</IconDiv>
-      {user && areOptionsVisible && (
-        <ul>
-          <MenuItem onClick={() => navigate("/profile")}>Profile</MenuItem>
-          <MenuItem onClick={handleLogoutClick} disabled={isLoggingOut}>
-            Logout
-          </MenuItem>
-        </ul>
-      )}
-      {/* {!user && ( // temporarily disable while I focus on main features for MVP, which are search and path with local storage.
-        <ButtonsDiv>
-          <Button variant="contained" onClick={() => navigate("/login")}>Log In</Button>
-          <Button variant="outlined" onClick={() => navigate("/signup")}>Sign Up</Button>
-        </ButtonsDiv>
-      )} */}
     </StyledHeader>
   );
 }
